90ee2897f4a9291ee7e69fbd94743f83ff29a3a5
Shane McDonough <shanemcdman@gmail.com>
2025-08-28 11:47:37 -0700

    add new testcases to test.py for caps and single digits

--- test.py
+++ test.py
@@ -9,5 +9,23 @@ class TextToNumTest(unittest.TestCase):
 	def test_exists(self):
 		text_to_num('three')
 
+	def test_single_digit(self):
+		for i, word in enumerate((
+			'zero',
+			'one',
+			'two',
+			'three',
+			'four',
+			'five',
+			'six',
+			'seven',
+			'eight',
+			'nine',
+		)):
+			self.assertEqual(text_to_num(word), i)
+
+	def test_caps(self):
+		self.assertEqual(text_to_num('THREE'), 3)
+
 if __name__ == '__main__':
 	unittest.main()
