faac276457770467e9a2156fb318586299049487
Shane McDonough <shanemcdman@gmail.com>
2025-08-28 11:42:55 -0700

    Create and import text_to_num function in text_to_num.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import unittest
+from text_to_num import text_to_num
 
 class TextToNumTest(unittest.TestCase):
 	'''meant to test text_to_num function'''
---
+++ text_to_num.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def text_to_num(text: str) -> int:
+	'''
+	:text: the number to be converted into an integer in plain english
+		e.g. one hundred
+		e.g. three thousand two hundred and fifty
+		case does not matter
+	:return: the number represented by the text
+	'''
+	return 0
